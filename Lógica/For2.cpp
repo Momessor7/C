fe23ae76158e58b2963c79553c66160d1e2aceb2
Gabriel Marconato Momesso <142452550+Momessor7@users.noreply.github.com>
2024-11-21 23:32:52 -0300

    Update For2.cpp

--- Lógica/For2.cpp
+++ Lógica/For2.cpp
@@ -7,24 +7,22 @@
 
 int main() {
     int temp[12];
-    int maiorTemp = -1000, menorTemp = 1000; // Inicializando com valores extremos para garantir que as temperaturas reais sejam encontradas
+    int maiorTemp = -1000, menorTemp = 1000;
     int mesMaior, mesMenor;
 
-    // Recebe as temperaturas de cada mês
     for (int i = 0; i < 12; i++) {
         printf("Preencha a temperatura do mes %d: \n", i + 1);
         scanf("%d", &temp[i]);
     }
 
-    //encontrando a maior e menor temperatura
     for (int i = 0; i < 12; i++) {
         if (temp[i] > maiorTemp) {
             maiorTemp = temp[i];
-            mesMaior = i; // Armazenando o mês (índice) onde ocorreu a maior temperatura
+            mesMaior = i;
         }
         if (temp[i] < menorTemp) {
             menorTemp = temp[i];
-            mesMenor = i; // Armazenando o mês (índice) onde ocorreu a menor temperatura
+            mesMenor = i;
         }
     }
 
@@ -37,4 +35,7 @@ int main() {
     };
 
     printf("\nA maior temperatura do ano foi no mês de %s.\n", meses[mesMaior]);
-    printf("A menor temperatura do ano foi no mês de %s.\n", meses[mesMe
+    printf("A menor temperatura do ano foi no mês de %s.\n", meses[mesMenor]);
+
+    return 0;
+}
