f7bd85a5edc7216101d7d54278afd6e617677965
Gabriel Marconato Momesso <142452550+Momessor7@users.noreply.github.com>
2024-11-21 23:30:19 -0300

    Update For2.cpp

--- Lógica/For2.cpp
+++ Lógica/For2.cpp
@@ -1,19 +1,36 @@
-//Faça um programa que receba a temperatura média de cada mês do ano,
-//armazenando-as em um vetor. Calcule e mostre a maior e a menor temperatura do ano e
-//em que mês ocorreram (mostrar o mês por extenso: 1 – janeiro, 2 – fevereiro...).
-//Desconsidere empates.
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-	int temp[12];
-	
-	for(int i = 0; i < 12; i++) {
-		printf("Preencha a temperatura do mes %d: \n", i+1);
-		scanf("%d", &temp[i]);
-	}
-	
-	for(int i = 0; i < 12; i++) {
-		printf("")
-	}
-}
+    int temp[12];
+    int maiorTemp = -1000, menorTemp = 1000; // Inicializando com valores extremos para garantir que as temperaturas reais sejam encontradas
+    int mesMaior, mesMenor;
+
+    // Recebe as temperaturas de cada mÃªs
+    for (int i = 0; i < 12; i++) {
+        printf("Preencha a temperatura do mes %d: \n", i + 1);
+        scanf("%d", &temp[i]);
+    }
+
+    //encontrando a maior e menor temperatura
+    for (int i = 0; i < 12; i++) {
+        if (temp[i] > maiorTemp) {
+            maiorTemp = temp[i];
+            mesMaior = i; // Armazenando o mÃªs (Ã­ndice) onde ocorreu a maior temperatura
+        }
+        if (temp[i] < menorTemp) {
+            menorTemp = temp[i];
+            mesMenor = i; // Armazenando o mÃªs (Ã­ndice) onde ocorreu a menor temperatura
+        }
+    }
+
+    printf("\nA maior temperatura foi %dÂ°C no mÃªs %d.\n", maiorTemp, mesMaior + 1);
+    printf("A menor temperatura foi %dÂ°C no mÃªs %d.\n", menorTemp, mesMenor + 1);
+
+    char *meses[] = {
+        "janeiro", "fevereiro", "marÃ§o", "abril", "maio", "junho",
+        "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+    };
+
+    printf("\nA maior temperatura do ano foi no mÃªs de %s.\n", meses[mesMaior]);
+    printf("A menor temperatura do ano foi no mÃªs de %s.\n", meses[mesMe
