ae3850a6f7726c54daf4612b1548c5d947fb6981
Gabriel Marconato Momesso <142452550+Momessor7@users.noreply.github.com>
2024-11-21 23:31:50 -0300

    Update For2.cpp

--- Lógica/For2.cpp
+++ Lógica/For2.cpp
@@ -1,3 +1,7 @@
+//Faça um programa que receba a temperatura média de cada mês do ano,
+//armazenando-as em um vetor. Calcule e mostre a maior e a menor temperatura do ano e
+//em que mês ocorreram (mostrar o mês por extenso: 1 – janeiro, 2 – fevereiro...).
+//Desconsidere empates.
 #include <stdio.h>
 #include <stdlib.h>
 
